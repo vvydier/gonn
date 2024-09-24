6576ef80b569565e328f9d8e90eda205f615762b
Vinod Vydier <vvydier@gmail.com>
2024-09-24 15:50:59 -0500

    updated .gitignore and run script

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
+#build the application
 go build
+
+#train using mnist dataset
 ./gonn -mnist train
+#test prediction
 ./gonn -mnist predict
 
-# test recursively
+# test recursively for handwritten input files
 i=1; while [ $i  -le  9 ]
 do
 ./gonn -file nums/$i.png
